c433d320357af94dead933919d641b140788de88
bum-hyun <ru_bryunak@naver.com>
2021-01-20 08:36:31 +0900

    Fix: server

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const { createServer } = require('http');
 const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
@@ -6,6 +7,17 @@ const typeDefs = require("../graphql/schemas");
 const resolvers = require("../graphql/resolvers");
 const context = require("../graphql/context");
 const app = express();
+const dotenv = require('dotenv');
+const redis = require("redis");
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const hpp = require('hpp');
+dotenv.config();
+
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
 
 app.use(cors());
 
@@ -20,6 +32,18 @@ const apolloServer = new ApolloServer({
     },
   },
 });
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+const sessionOption = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  store: new RedisStore({ client: redisClient }),
+};
 
 apolloServer.applyMiddleware({ app, path: "/api" });
 
