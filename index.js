7af9f9e59f8c48ef81bf5c1388d7f2047791f21d
bum-hyun <ru_bryunak@naver.com>
2021-01-20 11:37:30 +0900

    Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const server = require("./api/server");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3301;
 
 process.on("uncaughtException", () => {
   console.error(`${(new Date()).toUTCString()} uncaughtException`, err);
