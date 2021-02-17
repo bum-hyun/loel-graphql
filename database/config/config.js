5f80f6ffb8066f46b9c4dc2282dc723bd13ac5fb
bum-hyun <ru_bryunak@naver.com>
2021-02-17 16:34:28 +0900

    Test

--- database/config/config.js
+++ database/config/config.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    "username": "root",
+    "username": "loel",
     "password": process.env.SEQUELIZE_PASSWORD,
     "database": "loel",
     "host": "127.0.0.1",
     "dialect": "mysql",
   },
   test: {
-    "username": "root",
+    "username": "loel",
     "password": process.env.SEQUELIZE_PASSWORD,
     "database": "loel",
     "host": "127.0.0.1",
