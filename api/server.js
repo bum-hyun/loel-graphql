8d166d7bb35204a8eee85f8f176cee1a309e6970
김범현 <bumhyunkim@newin.co.kr>
2021-03-03 14:00:21 +0900

    image upload error - 3

--- api/server.js
+++ api/server.js
@@ -12,18 +12,23 @@ const uploadRouter = require("../routes/upload");
 const anyRouter = require("../routes/any");
 dotenv.config();
 
-const allowlist = ['https://loelblog.com', 'https://www.loelblog.com']
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }
-  } else {
-    corsOptions = { origin: false }
-  }
-  callback(null, corsOptions)
-}
+// const allowlist = ['https://loelblog.com', 'https://www.loelblog.com']
+// const corsOptionsDelegate = function (req, callback) {
+//   let corsOptions;
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true }
+//   } else {
+//     corsOptions = { origin: false }
+//   }
+//   callback(null, corsOptions)
+// }
 
-app.use(cors(corsOptionsDelegate));
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' ? /loelblog\.com$/ : true,
+  methods: 'OPTIONS,GET,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  credentials: true,
+}));
 
 const apolloServer = new ApolloServer({
   typeDefs,
