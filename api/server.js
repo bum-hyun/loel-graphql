40e4a94136e1f5747be3588d3ca0b2db0c7ce91c
김범현 <bumhyunkim@newin.co.kr>
2021-03-03 14:08:14 +0900

    image upload error - 4

--- api/server.js
+++ api/server.js
@@ -12,23 +12,23 @@ const uploadRouter = require("../routes/upload");
 const anyRouter = require("../routes/any");
 dotenv.config();
 
-// const allowlist = ['https://loelblog.com', 'https://www.loelblog.com']
-// const corsOptionsDelegate = function (req, callback) {
-//   let corsOptions;
-//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { origin: true }
-//   } else {
-//     corsOptions = { origin: false }
-//   }
-//   callback(null, corsOptions)
-// }
+const allowlist = ['https://loelblog.com', 'https://www.loelblog.com']
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }
+  } else {
+    corsOptions = { origin: false }
+  }
+  corsOptions = { 
+    ...corsOptions,   
+    methods: 'OPTIONS,GET,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true }
+  callback(null, corsOptions)
+}
 
-app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? /loelblog\.com$/ : true,
-  methods: 'OPTIONS,GET,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  credentials: true,
-}));
+app.use(cors(corsOptionsDelegate))
 
 const apolloServer = new ApolloServer({
   typeDefs,
