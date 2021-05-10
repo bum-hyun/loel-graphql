a3d9770ada5934aa1e0226008890ac3b67dc16d0
김범현 <bumhyunkim@newin.co.kr>
2021-05-10 14:51:45 +0900

    Feature: 다트 실험 중 - 2

--- routes/dart.js
+++ routes/dart.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const express = require('express');
 const fs = require('fs');
 const AdmZip = require("adm-zip");
+const convert = require('xml-js');
 
 const router = express.Router();
 
@@ -15,9 +16,16 @@ router.get('/', async (req, res) => {
   })
   await fs.writeFileSync("corpCode.zip", result.data);
   const zip = new AdmZip("corpCode.zip");
+  fs.unlinkSync("corpCode.zip");
   const zipEntries = zip.getEntries();
   zipEntries.forEach(function (zipEntry) {
-    console.log(zipEntry.getData().toString("utf8"));
+    var xmlToJson = JSON.parse(convert.xml2json(zipEntry.getData().toString("utf8"), {compact: true, spaces: 2}));
+    xmlToJson.result.list.map(item => {
+      if (item.stock_code._text) {
+        console.log(item.corp_name._text);
+      }
+    })
+    // fs.writeFileSync("corpCode.json", xmlToJson);
   });
   // console.log(result.data);
   // res.status(200).json()
