a0e894c68f6239c5f183b761ba6343304a1ea7a1
bum-hyun <ru_bryunak@naver.com>
2022-09-21 14:05:09 +0900

    fix: 데이터베이스 이름 수정

--- database/config/config.js
+++ database/config/config.js
@@ -4,21 +4,21 @@ module.exports = {
   development: {
     "username": "root",
     "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "loel",
+    "database": "blog",
     "host": "127.0.0.1",
     "dialect": "mysql",
   },
   test: {
     "username": "loel",
     "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "loel",
+    "database": "blog",
     "host": "127.0.0.1",
     "dialect": "mysql",
   },
   production: {
     "username": "loel",
     "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "loel",
+    "database": "blog",
     "host": "127.0.0.1",
     "dialect": "mysql",
     logging: false
